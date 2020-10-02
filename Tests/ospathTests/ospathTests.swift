f47fa28fdababcf2c810f754379e192ef3261fb0
luoheng <1301089462@qq.com>
2020-10-02 20:57:40 +0800

    add test for isabs, join, split

--- Tests/ospathTests/ospathTests.swift
+++ Tests/ospathTests/ospathTests.swift
@@ -1,15 +1,35 @@
 import XCTest
 @testable import ospath
 
-final class ospathTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ospath().text, "Hello, World!")
+final class PosixPathTests: XCTestCase {
+
+    func testIsabs() {
+        XCTAssertEqual(PosixPath.isabs(""), false)
+        XCTAssertEqual(PosixPath.isabs("/"), true)
+        XCTAssertEqual(PosixPath.isabs("/foo"), true)
+        XCTAssertEqual(PosixPath.isabs("/foo/bar"), true)
+        XCTAssertEqual(PosixPath.isabs("foo/bar"), false)
+    }
+
+    func testJoin() {
+        XCTAssertEqual(PosixPath.join("/foo", "bar", "/bar", "baz"),
+                         "/bar/baz")
+        XCTAssertEqual(PosixPath.join("/foo", "bar", "baz"), "/foo/bar/baz")
+        XCTAssertEqual(PosixPath.join("/foo/", "bar/", "baz/"),
+                         "/foo/bar/baz/")
+    }
+
+    func testSplit() {
+        XCTAssert(PosixPath.split("/foo/bar") == ("/foo", "bar"))
+        XCTAssert(PosixPath.split("/") == ("/", ""))
+        XCTAssert(PosixPath.split("foo") == ("", "foo"))
+        XCTAssert(PosixPath.split("////foo") == ("////", "foo"))
+        XCTAssert(PosixPath.split("//foo//bar") == ("//foo", "bar"))
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testIsabs", testIsabs),
+        ("testJoin", testJoin),
+        ("testSplit", testSplit),
     ]
 }
