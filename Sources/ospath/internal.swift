ac6699925e34ac22776449729c4eb7b5b2913b64
luoheng23 <1301089462@qq.com>
2020-10-02 22:06:03 +0800

    remove rIndexOf function

--- Sources/ospath/internal.swift
+++ Sources/ospath/internal.swift
@@ -6,13 +6,14 @@ internal class Path {
     static let fileManager = FileManager.default
 
     // if given string is not found, return startIndex
-    static func rIndexOf(_ path: String, _ sep: String) -> String.Index  {
-        let pos = path.lastIndex(where: { $0 == sep.first })
-        if let p = pos {
-            return path.index(after: p)
-        }
-        return path.startIndex
-    }
 
+    // split a path in root and extension
+    // The extension is everything starting at the last dot in the last
+    // pathname component; the root is everything before that.
+    // It is always true that root + ext == p.
 
+    // static func splitext(path: String, sep: String, altsep: String?, extsep: String) {
+    //     var sepIndex = rIndexOf(path, sep)
+
+    // }
 }
