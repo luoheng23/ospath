35f283ce45552c4b74ba274fc9a58aad945c6898
luoheng23 <1301089462@qq.com>
2020-10-04 19:14:14 +0800

    add rstrip, lstrip and strip method for string.subsequence

--- Sources/ospath/internal.swift
+++ Sources/ospath/internal.swift
@@ -50,3 +50,29 @@ internal class Path {
         return (path, "")
     }
 }
+
+extension String.SubSequence {
+    mutating func lstrip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        var idx = self.startIndex
+        while idx != self.endIndex && _set.contains(self[idx]) {
+            idx = self.index(after: idx)
+        }
+        self = self[idx...]
+    }
+
+    mutating func rstrip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        var idx = self.endIndex
+        while idx != self.startIndex && _set.contains(self[self.index(before: idx)]) {
+            idx = self.index(before: idx)
+        }
+        self = self[..<idx]
+    }
+
+    mutating func strip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        self.lstrip()
+        self.rstrip()
+    }
+}
