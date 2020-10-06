91cdabfd374b7e893ac17c8cde8c522dd9dd0a00
luoheng <1301089462@qq.com>
2020-10-06 18:15:59 +0800

    move splitext from Path to BasePath

--- Sources/ospath/internal.swift
+++ Sources/ospath/internal.swift
@@ -3,52 +3,6 @@ import Foundation
 internal class Path {
 
     static let fileManager = FileManager.default
-
-    // if given string is not found, return startIndex
-
-    // split a path in root and extension
-    // The extension is everything starting at the last dot in the last
-    // pathname component; the root is everything before that.
-    // It is always true that root + ext == p.
-    static func splitext(
-        path: String,
-        sep: String,
-        altsep: String?,
-        extsep: String
-    ) -> (
-        String, String
-    ) {
-        var sepIndex: String.Index
-        var sepIndexValid = true
-        if let pos = path.lastIndex(where: { $0 == sep.first }) {
-            sepIndex = pos
-        }
-        else {
-            sepIndex = path.startIndex
-            sepIndexValid = false
-        }
-        if let alt = altsep {
-            if let pos = path[sepIndex...].lastIndex(where: { $0 == alt.first })
-            {
-                sepIndex = pos
-                sepIndexValid = true
-            }
-        }
-
-        if let pos = path[sepIndex...].lastIndex(where: { $0 == extsep.first })
-        {
-            // skip all leading dots
-            var filenameIndex =
-                sepIndexValid ? path.index(after: sepIndex) : path.startIndex
-            while filenameIndex != pos {
-                if path[filenameIndex] != extsep.first {
-                    return (String(path[..<pos]), String(path[pos...]))
-                }
-                filenameIndex = path.index(after: filenameIndex)
-            }
-        }
-        return (path, "")
-    }
 }
 
 extension String.SubSequence {
--- Sources/ospath/path.swift
+++ Sources/ospath/path.swift
@@ -148,7 +148,7 @@ public class BasePath {
 
 extension BasePath {
     public class func splitext(_ path: String) -> (head: String, tail: String) {
-        return Path.splitext(
+        return _splitext(
             path: path,
             sep: sep,
             altsep: altsep,
@@ -431,5 +431,50 @@ extension BasePath {
             seen[p] = newPath
         }
         return (newPath, true)
+    }
+
+    // if given string is not found, return startIndex
+    // split a path in root and extension
+    // The extension is everything starting at the last dot in the last
+    // pathname component; the root is everything before that.
+    // It is always true that root + ext == p.
+    static func _splitext(
+        path: String,
+        sep: String,
+        altsep: String?,
+        extsep: String
+    ) -> (
+        String, String
+    ) {
+        var sepIndex: String.Index
+        var sepIndexValid = true
+        if let pos = path.lastIndex(where: { $0 == sep.first }) {
+            sepIndex = pos
+        }
+        else {
+            sepIndex = path.startIndex
+            sepIndexValid = false
+        }
+        if let alt = altsep {
+            if let pos = path[sepIndex...].lastIndex(where: { $0 == alt.first })
+            {
+                sepIndex = pos
+                sepIndexValid = true
+            }
+        }
+
+        if let pos = path[sepIndex...].lastIndex(where: { $0 == extsep.first })
+        {
+            // skip all leading dots
+            var filenameIndex =
+                sepIndexValid ? path.index(after: sepIndex) : path.startIndex
+            while filenameIndex != pos {
+                if path[filenameIndex] != extsep.first {
+                    return (String(path[..<pos]), String(path[pos...]))
+                }
+                filenameIndex = path.index(after: filenameIndex)
+            }
+        }
+        return (path, "")
     }
 }
