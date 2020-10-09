b918bf1264a819e4932998398ace8b29fa181034
luoheng <1301089462@qq.com>
2020-10-09 21:12:44 +0800

    move string and array extension to internal.swift

---
+++ Sources/ospath/internal.swift
@@ -0,0 +1,108 @@
+
+extension String.SubSequence {
+    mutating func lstrip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        var idx = self.startIndex
+        while idx != self.endIndex && _set.contains(self[idx]) {
+            idx = self.index(after: idx)
+        }
+        self = self[idx...]
+    }
+
+    mutating func rstrip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        var idx = self.endIndex
+        while idx != self.startIndex
+            && _set.contains(self[self.index(before: idx)])
+        {
+            idx = self.index(before: idx)
+        }
+        self = self[..<idx]
+    }
+
+    mutating func strip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        self.lstrip()
+        self.rstrip()
+    }
+}
+
+extension String {
+    mutating func lstrip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        var idx = self.startIndex
+        while idx != self.endIndex && _set.contains(self[idx]) {
+            idx = self.index(after: idx)
+        }
+        self = String(self[idx...])
+    }
+
+    mutating func rstrip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        var idx = self.endIndex
+        while idx != self.startIndex
+            && _set.contains(self[self.index(before: idx)])
+        {
+            idx = self.index(before: idx)
+        }
+        self = String(self[..<idx])
+    }
+
+    mutating func strip(_ _set: Set<Character> = []) {
+        guard !_set.isEmpty else { return }
+        self.lstrip()
+        self.rstrip()
+    }
+}
+
+extension Array where Element: Comparable {
+    static func > (_ s1: [Element], _ s2: [Element]) -> Bool {
+        let length = Swift.min(s1.count, s2.count)
+        for i in 0..<length {
+            if s1[i] > s2[i] {
+                return true
+            }
+            if s1[i] < s2[i] {
+                return false
+            }
+        }
+        return false
+    }
+
+    func minmax() -> (head: Element, tail: Element)? {
+        if count == 0 {
+            return nil
+        }
+        var s1 = self[0]
+        var s2 = self[0]
+        for i in 0..<count {
+            if self[i] > s1 {
+                s1 = self[i]
+            }
+            else if s2 > self[i] {
+                s2 = self[i]
+            }
+        }
+        return (s1, s2)
+    }
+
+}
+
+extension Array where Element == [String.SubSequence] {
+    func minmax() -> (head: [String.SubSequence], tail: [String.SubSequence])? {
+        if count == 0 {
+            return nil
+        }
+        var s1 = self[0]
+        var s2 = self[0]
+        for i in 0..<count {
+            if self[i] > s1 {
+                s1 = self[i]
+            }
+            else if s2 > self[i] {
+                s2 = self[i]
+            }
+        }
+        return (s1, s2)
+    }
+}
--- Sources/ospath/path.swift
+++ Sources/ospath/path.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public class Path {
-
+    
     public var path: String
     public lazy var cls = type(of: self)
 
@@ -65,9 +65,7 @@ public class Path {
         return path
     }
 
-    public class func join(_ basePath: String, _ toJoinedPaths: String...)
-        -> String
-    {
+    public class func join(_ basePath: String, _ toJoinedPaths: String...) -> String {
         return join(basePath, toJoinedPaths)
     }
 
@@ -414,114 +412,6 @@ extension Path {
     }
 }
 
-extension String.SubSequence {
-    mutating func lstrip(_ _set: Set<Character> = []) {
-        guard !_set.isEmpty else { return }
-        var idx = self.startIndex
-        while idx != self.endIndex && _set.contains(self[idx]) {
-            idx = self.index(after: idx)
-        }
-        self = self[idx...]
-    }
-
-    mutating func rstrip(_ _set: Set<Character> = []) {
-        guard !_set.isEmpty else { return }
-        var idx = self.endIndex
-        while idx != self.startIndex
-            && _set.contains(self[self.index(before: idx)])
-        {
-            idx = self.index(before: idx)
-        }
-        self = self[..<idx]
-    }
-
-    mutating func strip(_ _set: Set<Character> = []) {
-        guard !_set.isEmpty else { return }
-        self.lstrip()
-        self.rstrip()
-    }
-}
-
-extension String {
-    mutating func lstrip(_ _set: Set<Character> = []) {
-        guard !_set.isEmpty else { return }
-        var idx = self.startIndex
-        while idx != self.endIndex && _set.contains(self[idx]) {
-            idx = self.index(after: idx)
-        }
-        self = String(self[idx...])
-    }
-
-    mutating func rstrip(_ _set: Set<Character> = []) {
-        guard !_set.isEmpty else { return }
-        var idx = self.endIndex
-        while idx != self.startIndex
-            && _set.contains(self[self.index(before: idx)])
-        {
-            idx = self.index(before: idx)
-        }
-        self = String(self[..<idx])
-    }
-
-    mutating func strip(_ _set: Set<Character> = []) {
-        guard !_set.isEmpty else { return }
-        self.lstrip()
-        self.rstrip()
-    }
-}
-
-extension Array where Element: Comparable {
-    static func > (_ s1: [Element], _ s2: [Element]) -> Bool {
-        let length = Swift.min(s1.count, s2.count)
-        for i in 0..<length {
-            if s1[i] > s2[i] {
-                return true
-            }
-            if s1[i] < s2[i] {
-                return false
-            }
-        }
-        return false
-    }
-
-    func minmax() -> (head: Element, tail: Element)? {
-        if count == 0 {
-            return nil
-        }
-        var s1 = self[0]
-        var s2 = self[0]
-        for i in 0..<count {
-            if self[i] > s1 {
-                s1 = self[i]
-            }
-            else if s2 > self[i] {
-                s2 = self[i]
-            }
-        }
-        return (s1, s2)
-    }
-
-}
-
-extension Array where Element == [String.SubSequence] {
-    func minmax() -> (head: [String.SubSequence], tail: [String.SubSequence])? {
-        if count == 0 {
-            return nil
-        }
-        var s1 = self[0]
-        var s2 = self[0]
-        for i in 0..<count {
-            if self[i] > s1 {
-                s1 = self[i]
-            }
-            else if s2 > self[i] {
-                s2 = self[i]
-            }
-        }
-        return (s1, s2)
-    }
-}
-
 extension Path {
 
     public func join(_ toJoinedPaths: String...) -> Path {
@@ -577,13 +467,11 @@ extension Path {
     public var isExecutable: Bool { return cls.isExecutable(path) }
     public var isDeletable: Bool { return cls.isDeletable(path) }
 
-    func samefile(_ path: String) -> Bool {
-        return cls.samefile(self.path, path)
-    }
+    func samefile(_ path: String) -> Bool { return cls.samefile(self.path, path) }
 }
 
 extension Path: Equatable {
-    public static func == (lhs: Path, rhs: Path) -> Bool {
+    public static func ==(lhs: Path, rhs: Path) -> Bool {
         return lhs.path == rhs.path
     }
 }
@@ -595,11 +483,11 @@ extension Path: Comparable {
 }
 
 extension Path {
-    public static func + (lhs: Path, rhs: Path) -> Path {
+    public static func +(lhs: Path, rhs: Path) -> Path {
         return lhs.join(rhs)
     }
 
-    public static func + (lhs: Path, rhs: String) -> Path {
+    public static func +(lhs: Path, rhs: String) -> Path {
         return lhs.join(rhs)
     }
 
@@ -613,7 +501,5 @@ extension Path {
 }
 
 extension Path: CustomStringConvertible {
-    public var description: String {
-        return "\(String(describing: self))(\"\(path)\")"
-    }
+    public var description: String { return "\(String(describing: self))(\"\(path)\")"}
 }
